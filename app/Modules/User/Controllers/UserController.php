5d889f3d0e68ef2e6e4a1ae258ddfb3a74f2dad0
khanht <khanh.t@citigo.com.vn>
2019-07-29 16:22:36 +0700

    update

---
+++ app/Modules/User/Controllers/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Modules\User\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+
+    public function login()
+    {
+        dd(request()->all());
+    }
+}
---
+++ app/Modules/User/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+$module_namespace = "App\Modules\User\Controllers";
+
+Route::namespace($module_namespace)->group(function () {
+    Route::middleware(['cors'])->group(function() {
+        Route::post('/user/login', "UserController@login")->name('user.login');
+    });
+});
